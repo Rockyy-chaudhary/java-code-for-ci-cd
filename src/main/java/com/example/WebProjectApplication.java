503af80df5121133e95e5877cd5e59aa1e0f1eec
Rockyy-chaudhary <rockey2597@gmail.com>
2026-09-22 00:18:35 +0530

    Update WebProjectApplication.java by rocky

--- src/main/java/com/example/WebProjectApplication.java
+++ src/main/java/com/example/WebProjectApplication.java
@@ -18,7 +18,7 @@ public class WebProjectApplication {
                 "<head><title>CloudFolks HUB</title></head>" +
                 "<body style='text-align:center; background-color:#f0f8ff;'>" +
                 "<h1 style='color: #4CAF50;'>Welcome to <span style='color: #00008B;'>CloudFolks HUB</span>!</h1>" +
-                "<p style='font-size:20px; color: #555;'>Empowering Your DevOps Journey</p>" +
+                "<p style='font-size:20px; color: #555;'>Empowering Your DevOps Journey by rocky and atul</p>" +
                 "<form id='form' method='post' action='/submit' style='margin-top:20px;'>" +
                 "<label for='name'>Name:</label><br>" +
                 "<input type='text' id='name' name='name' required><br><br>" +
